00336d14f7195897e285c7b864728902e458aa05
repl.it user <replituser@example.com>
2020-09-09 01:15:24 +0000

    hello

--- printing/printing.py
+++ printing/printing.py
@@ -3,6 +3,10 @@ def print_examples():
     m = 4
     f = 42.7
     # place the print calls specified in the lab description below here
-
+    print ("Entered print_examples function.\n")
+    print ("n:", n)
+    n = 99
+    print ("n:", n, "\t m:", m)
+    print ("f: ", f)
 if __name__ == '__main__':
     print_examples()
